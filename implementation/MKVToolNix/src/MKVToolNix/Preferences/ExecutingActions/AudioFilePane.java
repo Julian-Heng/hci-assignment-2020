5266a05ed3fd39c42b2d60f4c85dc4552f349d12
julian-heng <julianhengwl@gmail.com>
2020-05-13 01:52:38 +0800

    File picker on audio file in executing actions

--- implementation/MKVToolNix/src/MKVToolNix/Preferences/ExecutingActions/AudioFilePane.java
+++ implementation/MKVToolNix/src/MKVToolNix/Preferences/ExecutingActions/AudioFilePane.java
@@ -6,6 +6,11 @@
 package MKVToolNix.Preferences.ExecutingActions;
 
 import MKVToolNix.CustomAnchorPane;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 
 /**
@@ -14,8 +19,19 @@ import MKVToolNix.CustomAnchorPane;
  */
 public class AudioFilePane extends CustomAnchorPane
 {
+    @FXML
+    private TextField txtFile;
+    @FXML
+    private Button btnFilePicker;
+
+
     public AudioFilePane()
     {
         super("Preferences/ExecutingActions/AudioFilePane.fxml");
+
+        btnFilePicker.setOnAction(e ->
+        {
+            txtFile.setText((new FileChooser()).showOpenDialog(((Stage)getScene().getWindow())).getName());
+        });
     }
 }
